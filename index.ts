67c847673f0da184f50a4a1ce8130e557938e9fb
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-03-30 09:46:03 -0500

    removed array of values, going to stick with function and randnum with Math.random vs looping through an array and randomly picking an index

--- index.ts
+++ index.ts
@@ -1,20 +1,16 @@
-console.log('hello world from ts')
-
-const cpuChoices = ['rock', 'paper', 'scissors'];
-
-const getCpuChoice = () => {
+const getCpuChoice = (): string => {
     // first get random number:
-    const randNum = (Math.floor(Math.random() * cpuChoices.length) + 1);
+    const randNum: number = (Math.floor(Math.random() * 3) + 1);
     // console.log(randNum)
     // initiate finalchoice var
-    let finalChoice;
+    let finalChoice: string;
     randNum === 1 ? finalChoice = 'rock' :
     randNum === 2 ? finalChoice = 'paper' :
-    randNum === 3 ? finalChoice = 'scissors':
-    randNum === null;
-    // check through all the cases, maybe use switch 
-    // statment ? seems to work for now, default to null 
+    finalChoice = 'scissors'
     console.log(finalChoice)
+    return finalChoice!;
+    // check through all the cases, maybe use switch 
+    // statement?
 }
 
 
