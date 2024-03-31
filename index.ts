162beff79c78a4caca222c7910d4e7fce685c3b9
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-03-31 09:27:40 -0500

    added the getPlayerChoice inside evt listeners for player selection, and set up the comparison check in determine winner

--- index.ts
+++ index.ts
@@ -2,19 +2,29 @@ const rock: HTMLElement = document.getElementById('rock')!;
 const paper: HTMLElement = document.getElementById('paper')!;
 const scissors: HTMLElement = document.getElementById('scissors')!;
 
+let playerChoice: string = '';
+
+const getPlayerChoice = (choice: string) => {
+    playerChoice = choice;
+    console.log(`Player choice is ${choice}`)
+    playGame();
+};
+
+// listen for player choice, and call getPlayerChoice,
+// passing in the respected value
 rock.addEventListener('click', () => {
-    console.log('clicked rock')
+    console.log('clicked rock');
+    getPlayerChoice('rock');
 });
 paper.addEventListener('click', () => {
-    console.log('clicked paper')
+    console.log('clicked paper');
+    getPlayerChoice('paper');
 });
 scissors.addEventListener('click', () => {
-    console.log('clicked scissors')
+    console.log('clicked scissors');
+    getPlayerChoice('scissors');
 });
 
-const getPlayerChoice = () => {
-
-};
 
 
 // CPU selection:
@@ -28,12 +38,13 @@ const getCpuChoice = (): string => {
     randNum === 2 ? finalChoice = 'paper' :
     finalChoice = 'scissors'
     console.log(finalChoice)
-    return finalChoice!;
+    return finalChoice;
     // check through all the cases, maybe use switch 
     // statement?
 }
-getCpuChoice();
+
 
 const playGame = () => {
-
+    const cpuChoice = getCpuChoice();
+    
 }
--- js/index.js
+++ js/index.js
@@ -2,14 +2,25 @@
 var rock = document.getElementById('rock');
 var paper = document.getElementById('paper');
 var scissors = document.getElementById('scissors');
+var playerChoice = '';
+var getPlayerChoice = function (choice) {
+    playerChoice = choice;
+    console.log("Player choice is ".concat(choice));
+    playGame();
+};
+// listen for player choice, and call getPlayerChoice,
+// passing in the respected value
 rock.addEventListener('click', function () {
     console.log('clicked rock');
+    getPlayerChoice('rock');
 });
 paper.addEventListener('click', function () {
     console.log('clicked paper');
+    getPlayerChoice('paper');
 });
 scissors.addEventListener('click', function () {
     console.log('clicked scissors');
+    getPlayerChoice('scissors');
 });
 // CPU selection:
 var getCpuChoice = function () {
@@ -26,4 +37,6 @@ var getCpuChoice = function () {
     // check through all the cases, maybe use switch 
     // statement?
 };
-getCpuChoice();
+var playGame = function () {
+    var cpuChoice = getCpuChoice();
+};
